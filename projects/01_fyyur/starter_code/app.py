0fa889f6d116f467c17bf0ecb94fd50e5e35aa97
Eduardo Silva <eduardo.susan@gmail.com>
2020-06-12 07:15:37 -0600

    finished venue edit form and view

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -5,7 +5,7 @@
 import json
 import dateutil.parser
 import babel
-from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask import Flask, render_template, request, Response, flash, redirect, url_for, jsonify
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 import logging
@@ -248,23 +248,41 @@ def delete_venue(venue_id):
 
   # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
   # clicking that button delete it from the db then redirect the user to the homepage
-  return None
+  error = False
+  response = True
+  body = []
+  try:
+    venue = Venue.query.get(venue_id)
+    body = {
+        "id"   : venue.id,
+        "name" : venue.name
+    }
+    db.session.delete(venue)
+    db.session.commit()
+  except:
+    db.session.rollback()
+    print(sys.exc_info())
+    error = True
+  finally:
+    db.session.close()
+    if error:
+      response = False
+      # on error, flash error message
+      flash('An error occurred. Venue id:' + str(venue_id) + ' could not be removed!')
+      body['error'] = error
+    else:
+      # on successful db delete, flash success
+      flash('Venue id:' + str(venue_id) + ' was successfully removed!')
+      body['response'] = response
+
+  return jsonify(body)
 
 #  Artists
 #  ----------------------------------------------------------------
 @app.route('/artists')
 def artists():
   # DONE: replace with real data returned from querying the database
-  # data=[{
-  #   "id": 4,
-  #   "name": "Guns N Petals",
-  # }, {
-  #   "id": 5,
-  #   "name": "Matt Quevedo",
-  # }, {
-  #   "id": 6,
-  #   "name": "The Wild Sax Band",
-  # }]
+  
   data = Artist.query.order_by(Artist.name).all()
   
   return render_template('pages/artists.html', artists=data)
@@ -356,28 +374,73 @@ def edit_artist_submission(artist_id):
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
-  form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
-  # TODO: populate form with values from venue with ID <venue_id>
+  # form = VenueForm()
+  # venue={
+  #   "id": 1,
+  #   "name": "The Musical Hop",
+  #   "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
+  #   "address": "1015 Folsom Street",
+  #   "city": "San Francisco",
+  #   "state": "CA",
+  #   "phone": "123-123-1234",
+  #   "website": "https://www.themusicalhop.com",
+  #   "facebook_link": "https://www.facebook.com/TheMusicalHop",
+  #   "seeking_talent": True,
+  #   "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
+  #   "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+  # }
+  # # TODO: populate form with values from venue with ID <venue_id>
+  venue = Venue.query.get(venue_id)
+  form = VenueForm(obj=venue)
+  form.genres.data = venue.genres.split(',')
+
   return render_template('forms/edit_venue.html', form=form, venue=venue)
+
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
   # TODO: take values from the form submitted, and update existing
   # venue record with ID <venue_id> using the new attributes
+  error = False
+  venue = Venue.query.get(venue_id)
+  # form = VenueForm(obj=venue)
+  form = VenueForm()
+
+  try:
+    tmp_genres = form.genres.data
+    if form.seeking_talent.data == 'True':
+      venue.seeking_talent = True
+    else:
+      venue.seeking_talent = False
+   
+    venue.name=form.name.data,
+    venue.city=form.city.data,
+    venue.state=form.state.data,
+    venue.address=form.address.data,
+    venue.phone=form.phone.data,
+    venue.genres=','.join(tmp_genres),
+    venue.website=form.website.data,
+    venue.facebook_link=form.facebook_link.data,
+    venue.image_link=form.image_link.data,
+    venue.seeking_description=form.seeking_description.data
+
+    db.session.add(venue)
+    db.session.commit()
+
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+    if error:
+        # on error, flash error message
+        flash('An error occurred. Venue ' + request.form['name'] + ' could not be updated.')
+
+    else:
+        # on successful db insert, flash success
+        flash('Venue ' + request.form['name'] + ' was successfully udated!')
+  
   return redirect(url_for('show_venue', venue_id=venue_id))
 
 #  Create Artist
