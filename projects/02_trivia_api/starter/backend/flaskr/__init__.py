b4998b671069740fc6337d9638b5b1e758768959
Eduardo Silva <eduardo.susan@gmail.com>
2020-08-01 18:19:22 -0600

    added node_modules to gitignore

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -45,7 +45,7 @@ def create_app(test_config=None):
   @app.route('/categories', methods=['GET'])
   def get_categories():
     selection = Category.query.order_by(Category.id).all()
-    categs = [categ.format() for categ in selection]
+    categs = [categ.type for categ in selection]
     
     if len(selection) == 0:
       abort(404)
@@ -62,49 +62,75 @@ def create_app(test_config=None):
 
 
   '''
-  @TODO: 
+  DONE @TODO: 
   Create an endpoint to handle GET requests for questions, 
   including pagination (every 10 questions). 
   This endpoint should return a list of questions, 
   number of total questions, current category, categories. 
 
-  TEST: At this point, when you start the application
+  todo>> TEST: At this point, when you start the application
   you should see questions and categories generated,
   ten questions per page and pagination at the bottom of the screen for three pages.
   Clicking on the page numbers should update the questions. 
   '''
   @app.route('/questions', methods=['GET'])
   def get_questions():
-    selection = Question.query.order_by(Question.id).all()
-    paginated_questions = paginate(request, selection)
-    categories = Category.query.order_by(Category.id).all()
+    try:
+      selection = Question.query.order_by(Question.id).all()
+      paginated_questions = paginate(request, selection)
+      categories = Category.query.order_by(Category.id).all()
 
-# My TODO: i don't know if the categories should return a number
-# Maybe the frontend will give hints
-    if len(paginated_questions) == 0:
-      abort(404)
+  # My TODO: i don't know if the categories should return a number
+  # Maybe the frontend will give hints
+      if len(paginated_questions) == 0:
+        abort(404)
 
-    full_list = [c.type for c in categories]
+      full_list = [c.type for c in categories]
 
 
-    return jsonify({
-      'success': True,
-      'questions': paginated_questions,
-      'categories': full_list,
-      'current_category': categories[0].type,
-      'total_questions': len(selection)
-    })
+      return jsonify({
+        'success': True,
+        'questions': paginated_questions,
+        'categories': full_list,
+        'current_category': categories[0].type,
+        'total_questions': len(selection)
+      })
+
+    except:
+      abort(422)
 
 
 
 
   '''
-  @TODO: 
+  DONE @TODO: 
   Create an endpoint to DELETE question using a question ID. 
 
-  TEST: When you click the trash icon next to a question, the question will be removed.
+  todo>> TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page. 
   '''
+  # MyTodo: File a bug about getQuestions() function when the last question delete removes a page
+  @app.route('/questions/<int:question_id>', methods=['DELETE'])
+  def delete_question(question_id):
+    try:
+      question = Question.query.filter(Question.id == question_id).one_or_none()
+
+      if question is None:
+        abort(404)
+
+      question.delete()
+      selection = Question.query.order_by(Question.id).all()
+      current_questions = paginate(request, selection)
+
+      return jsonify({
+        'success': True,
+        'deleted': question_id,
+        'questions': current_questions,
+        'total_questions': len(selection)
+      })
+    
+    except:
+      abort(422)
 
   '''
   @TODO: 
@@ -116,6 +142,32 @@ def create_app(test_config=None):
   the form will clear and the question will appear at the end of the last page
   of the questions list in the "List" tab.  
   '''
+  @app.route('/questions', methods=['POST'])
+  def create_question():
+    body = request.get_json()
+    
+    new_question = body.get('question', None)
+    new_answer = body.get('answer', None)
+    new_category = body.get('category', None)
+    new_difficulty = body.get('difficulty', None)
+    if not all([new_question, new_answer, new_category, new_difficulty]):
+      abort(400)
+
+    try:
+      q = Question(question=new_question, answer=new_answer, category=new_category, difficulty=new_difficulty)
+      q.insert()
+      selection = Question.query.order_by(Question.id).all()
+      paginated_questions = paginate(request, selection)
+
+      return jsonify({
+        'success': True,
+        'created': q.id,
+
+      })
+
+    except:
+      abort(422)
+
 
   '''
   @TODO: 
@@ -127,6 +179,7 @@ def create_app(test_config=None):
   only question that include that string within their question. 
   Try using the word "title" to start. 
   '''
+
 
   '''
   @TODO: 
@@ -151,11 +204,42 @@ def create_app(test_config=None):
   '''
 
   '''
-  @TODO: 
+  DONE @TODO: 
   Create error handlers for all expected errors 
   including 404 and 422. 
   '''
-  
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({
+      'success': False,
+      'error': 404,
+      'message': 'resource not found'
+    }), 404
+
+  @app.errorhandler(422)
+  def unprocessable(error):
+    return jsonify({
+      'success': False,
+      'error': 422,
+      'message': 'Unprocessable. I cannot figure out what you mean.'
+    }), 422
+
+  @app.errorhandler(405)
+  def not_allowed(error):
+    return jsonify({
+      'success': False,
+      'error': 405,
+      'message': 'method not allowed'
+    }), 405  
+
+  @app.errorhandler(400)
+  def bad_request(error):
+    return jsonify({
+      'success': False,
+      'error': 400,
+      'message': 'bad request'
+    }), 400
+
   return app
 
     
